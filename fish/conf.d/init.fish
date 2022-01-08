320bf15fe969ccc1ce933d7b23b4a83828fbe509
lyhokia <lyhokia@gmail.com>
2022-01-08 10:33:53 +0800

    Update environment variables in login shell

--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -4,13 +4,9 @@ set -gx HOME_LANG            zh
 # Path
 set -gxa PATH                ~/Scripts
 set -gxa PATH                ~/.local/bin
-# set -gxa PATH                ~/.nimble/bin
+set -gxa PATH                ~/.nimble/bin
 set -gxa PATH                ~/.yarn/bin
 set -gxa PATH                ~/.cargo/bin
-set -gxa PATH                ~/.emacs.d/bin
-for i in (luarocks path | awk '{sub(/PATH=/, "PATH ", $2); print "set -gx "$2}'); eval $i; end
-set -gxa PATH                /usr/bin/core_perl
-set -gxa PATH                /usr/bin/vendor_perl
 
 # XDG
 set -gx  XDG_CONFIG_HOME     ~/.config
@@ -29,9 +25,6 @@ set -gx MOZ_ENABLE_WAYLAND   1
 set GTK_IM_MODULE            fcitx
 set QT_IM_MODULE             fcitx
 set XMODIFIERS               \@im=fcitx
-
-# JANET
-set JANET_LIBPATH            ~/.local/lib/janet
 
 # Spawning sway
 set TTY1 (tty)
