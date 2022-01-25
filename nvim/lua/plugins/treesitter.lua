a63c8d61db053e4a5bce1feffc64ac90f49cc577
lyhokia <lyhokia@gmail.com>
2022-01-25 21:00:39 +0800

    Add fish, common-lisp to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -25,14 +25,24 @@ return function()
       },
   }
 
+  parser_configs.commonlisp = {
+    install_info = {
+      url = "https://github.com/theHamsta/tree-sitter-commonlisp",
+      files = { "src/parser.c" },
+      generate_requires_npm = true,
+    },
+    filetype = "lisp",
+    maintainers = { "@theHamsta" },
+  }
+
   for _, p in pairs(require('nvim-treesitter.parsers').get_parser_configs()) do
-    p.install_info.url = p.install_info.url:gsub('github.com', GITHUB_CDN)
+    p.install_info.url = p.install_info.url:gsub('github.com', _G.GITHUB_CDN)
   end
 
   require('nvim-treesitter.configs').setup({
     ensure_installed = {
       'c', 'cpp', 'rust', 'lua', 'python', 'norg', 'norg_meta', 'norg_table',
-      'fennel', 'query'
+      'fennel', 'query', 'commonlisp', 'fish'
     },
     highlight = {
       enable = true,
