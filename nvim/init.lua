4bda2e0b81001652cc52ea50a782e9d0750ba750
glyh <lyhokia@gmail.com>
2022-03-21 11:42:55 +0800

    format: indent

--- nvim/init.lua
+++ nvim/init.lua
@@ -177,20 +177,20 @@ require('packer').startup({function(use)
   use { 'folke/todo-comments.nvim',
     config = function ()
       require('todo-comments').setup {
-          keywords = {
-            FIX = {
-              icon = " ", -- icon used for the sign, and in search results
-              color = "error", -- can be a hex color, or a named color (see below)
-              alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-              -- signs = false, -- configure signs for some keywords individually
-            },
-            TODO = { icon = " ", color = "info" },
-            OK = { icon = " ", color = "info" },
-            HACK = { icon = " ", color = "warning" },
-            WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" , "ERROR"} },
-            PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-            NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
           },
+          TODO = { icon = " ", color = "info" },
+          OK = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" , "ERROR"} },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        },
       }
     end,
   }
