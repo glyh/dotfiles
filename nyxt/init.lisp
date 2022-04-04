1210986470213d13cd2c3661a590adad22278742
glyh <lyhokia@gmail.com>
2022-04-04 21:28:29 +0800

    remove redundent codes

--- nyxt/init.lisp
+++ nyxt/init.lisp
@@ -1,18 +1,5 @@
 (in-package #:nyxt-user)
 
 ;; Import Files
-(dolist (file (list (nyxt-init-file "statusline.lisp")
-                    (nyxt-init-file "stylesheet.lisp")))
-  (load file))
-
-#|
-(define-configuration web-buffer
-  ((gtk-extensions-path
-    (make-instance 'gtk-extensions-data-path
-                   :dirname "~/.config/nyxt_repo/libraries/web-extensions"))))
-
-(nyxt/web-extensions:load-web-extension beastify "/home/lyh/.config/webextensions-examples/beastify/")
-
-(define-configuration web-buffer
-  ((default-modes `(beastify ,@%slot-default%))))
-|#
+(nyxt::load-lisp "~/.config/nyxt/statusline.lisp")
+(nyxt::load-lisp "~/.config/nyxt/stylesheet.lisp")
