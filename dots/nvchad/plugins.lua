7a161c89a19306d0f8ccb3ae40f5435d6bf48b18
glyh <lyhokia@gmail.com>
2023-06-27 12:19:38 +0800

    upd

--- dots/nvchad/plugins.lua
+++ dots/nvchad/plugins.lua
@@ -160,6 +160,29 @@ local plugins = {
     end
   },
 
+  { 'nvim-neorg/neorg',
+    enabled = false,
+    cmd = "Neorg",
+    ft = 'neorg',
+    build = ':Neorg sync-parsers',
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/notes",
+              },
+            },
+          },
+        },
+      }
+    end
+  },
+
   -- }}}
 
   -- Lisp {{{
