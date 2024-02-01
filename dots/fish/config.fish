e5e9c4903978e81c5f42c51226fff49d00edfba1
glyh <lyhokia@gmail.com>
2024-02-01 13:02:18 +0800

    stop using vim cursor as it regress with tide

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,11 +1,11 @@
 # Fish options
 fish_vi_key_bindings
 set fish_greeting
-set -x fish_cursor_default block
-set -x fish_cursor_insert line blink
-set -x fish_cursor_replace_one underscore
-set -x fish_cursor_visual block
-set fish_vi_force_cursor
+# set -x fish_cursor_default block
+# set -x fish_cursor_insert line blink
+# set -x fish_cursor_replace_one underscore
+# set -x fish_cursor_visual block
+# set fish_vi_force_cursor
 
 # Plugins
 # type -q starship; and starship init fish | source # Use starship
