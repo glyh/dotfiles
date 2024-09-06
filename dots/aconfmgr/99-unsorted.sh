2452d5ca59afa4614ca3a089398102926409c2fc
glyh <lyhokia@gmail.com>
2024-09-06 11:22:24 +0800

    update

--- dots/aconfmgr/99-unsorted.sh
+++ dots/aconfmgr/99-unsorted.sh
@@ -1,164 +1,161 @@
 
 
-# Sat Aug 24 09:30:56 CST 2024 - Unknown packages
+# Fri Sep  6 11:21:07 CST 2024 - Unknown packages
 
 
-AddPackage adobe-source-han-sans-cn-fonts # Adobe Source Han Sans Subset OTF - Simplified Chinese OpenType/CFF fonts
-AddPackage adobe-source-han-serif-cn-fonts # Adobe Source Han Serif Subset OTF - Simplified Chinese OpenType/CFF fonts
-AddPackage aria2 # Download utility that supports HTTP(S), FTP, BitTorrent, and Metalink
-AddPackage atool # A script for managing file archives of various types
-AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage adobe-source-han-sans-cn-fonts # Adobe Source Han Sans Subset OTF -
+AddPackage adobe-source-han-serif-cn-fonts # Adobe Source Han Serif Subset OTF -
+AddPackage aria2 # Download utility that supports HTTP(S),
+AddPackage atool # A script for managing file archives of
+AddPackage base # Minimal package set to define a basic Arch
 AddPackage base-devel # Basic tools to build Arch Linux packages
-AddPackage bear # A tool to generate compilation database for clang tooling
+AddPackage bear # A tool to generate compilation database
 AddPackage blueman # GTK+ Bluetooth Manager
-AddPackage breeze # Artwork, styles and assets for the Breeze visual style for the Plasma Desktop
+AddPackage breeze # Artwork, styles and assets for the Breeze
 AddPackage clang # C language family frontend for LLVM
-AddPackage corkscrew # A tool for tunneling SSH through HTTP proxies
-AddPackage direnv # A shell extension that manages your environment
+AddPackage corkscrew # A tool for tunneling SSH through HTTP
+AddPackage direnv # A shell extension that manages your
 AddPackage dust # A more intuitive version of du in rust
 AddPackage easyeffects # Audio Effects for Pipewire applications
-AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
-AddPackage emacs # The extensible, customizable, self-documenting real-time display editor
-AddPackage eza # A modern replacement for ls (community fork of exa)
-AddPackage fastfetch # Like Neofetch, but much faster because written in C
-AddPackage fcitx5-chinese-addons # Addons related to Chinese, including IME previous bundled inside fcitx4
+AddPackage efibootmgr # Linux user-space application to modify the
+AddPackage emacs # The extensible, customizable,
+AddPackage eza # A modern replacement for ls (community
+AddPackage fastfetch # An actively maintained, feature-rich and
+AddPackage fcitx5-chinese-addons # Addons related to Chinese, including IME
 AddPackage fcitx5-configtool # Configuration Tool for Fcitx5
-AddPackage fcitx5-gtk # Fcitx5 gtk im module and glib based dbus client library
+AddPackage fcitx5-gtk # Fcitx5 gtk im module and glib based dbus
 AddPackage fcitx5-qt # Fcitx5 Qt Library (Qt5 & Qt6 integrations)
-AddPackage fish # Smart and user friendly shell intended mostly for interactive use
-AddPackage gamemode # A daemon/lib combo that allows games to request a set of optimisations be temporarily applied to the
-AddPackage git # the fast distributed version control system
-AddPackage git-delta # Syntax-highlighting pager for git and diff output
+AddPackage fish # Smart and user friendly shell intended
+AddPackage gamemode # A daemon/lib combo that allows games to
+AddPackage git # the fast distributed version control
+AddPackage git-delta # Syntax-highlighting pager for git and diff
 AddPackage git-lfs # Git extension for versioning large files
-AddPackage go # Core compiler tools for the Go programming language
-AddPackage gopass-jsonapi # jsonapi for gopass used for bridging to browsers
+AddPackage go # Core compiler tools for the Go programming
+AddPackage gopass-jsonapi # jsonapi for gopass used for bridging to
 AddPackage graphviz # Graph visualization software
 AddPackage grim # Screenshot utility for Wayland
 AddPackage grub # GNU GRand Unified Bootloader (2)
-AddPackage hyprland # a highly customizable dynamic tiling Wayland compositor
+AddPackage hyprland # a highly customizable dynamic tiling
 AddPackage imagemagick # An image viewing/manipulation program
 AddPackage intel-ucode # Microcode update files for Intel CPUs
 AddPackage jellyfin-ffmpeg # Jellyfin fork of ffmpeg
 AddPackage jellyfin-server # Jellyfin server backend
 AddPackage jellyfin-web # Web client for Jellyfin
 AddPackage jq # Command-line JSON processor
-AddPackage julia # High-level, high-performance, dynamic programming language
-AddPackage kdenlive # A non-linear video editor for Linux using the MLT video framework
-AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal emulator
+AddPackage julia # High-level, high-performance, dynamic
+AddPackage kdenlive # A non-linear video editor for Linux using
+AddPackage kitty # A modern, hackable, featureful,
 AddPackage kooha # Elegantly record your screen
-AddPackage lib32-fontconfig # Library for configuring and customizing font access
+AddPackage lib32-fontconfig # Library for configuring and customizing
 AddPackage lib32-nvidia-utils # NVIDIA drivers utilities (32-bit)
-AddPackage lib32-pipewire # Low-latency audio/video router and processor - 32-bit
-AddPackage lib32-pipewire-jack # Low-latency audio/video router and processor - 32-bit - JACK support
-AddPackage libreoffice-fresh # LibreOffice branch which contains new features and program enhancements
+AddPackage lib32-pipewire # Low-latency audio/video router and
+AddPackage lib32-pipewire-jack # Low-latency audio/video router and
 AddPackage linux # The Linux kernel and modules
 AddPackage linux-firmware # Firmware files for Linux
 AddPackage lldb # Next generation, high-performance debugger
 AddPackage llvm # Compiler infrastructure
 AddPackage lsp-plugins-lv2 # Collection of open-source plugins - LV2
 AddPackage ly # TUI display manager
-AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage mako # Lightweight notification daemon for
 AddPackage man-db # A utility for reading man pages
 AddPackage man-pages # Linux man pages
 AddPackage mold # A Modern Linker
-AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
-AddPackage networkmanager # Network connection manager and user applications
+AddPackage neovim # Fork of Vim aiming to improve user
+AddPackage networkmanager # Network connection manager and user
 AddPackage noto-fonts # Google Noto TTF fonts
 AddPackage noto-fonts-emoji # Google Noto emoji fonts
 AddPackage npm # JavaScript package manager
 AddPackage ntfs-3g # NTFS filesystem driver and utilities
 AddPackage nvidia # NVIDIA drivers for linux
-AddPackage obs-studio # Free, open source software for live streaming and recording
-AddPackage ollama # Create, run and share large language models (LLMs)
+AddPackage obs-studio # Free, open source software for live
+AddPackage ollama # Create, run and share large language
 AddPackage opam # OCaml package manager
-AddPackage openssh # SSH protocol implementation for remote login, command execution and file transfer
-AddPackage p7zip # Command-line file archiver with high compression ratio
+AddPackage openssh # SSH protocol implementation for remote
+AddPackage p7zip # Command-line file archiver with high
 AddPackage pamixer # Pulseaudio command-line mixer like amixer
 AddPackage pandoc-cli # Conversion between documentation formats
-AddPackage perl-ipc-system-simple # Run commands simply, with detailed diagnostics
-AddPackage perl-string-shellquote # Quote strings for passing through the shell
-AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
-AddPackage pipewire-audio # Low-latency audio/video router and processor - Audio support
-AddPackage pipewire-jack # Low-latency audio/video router and processor - JACK replacement
-AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
-AddPackage podman # Tool and library for running OCI-based containers in pods
-AddPackage podman-compose # A script to run docker-compose.yml using podman
-AddPackage progress # Shows running coreutils basic commands and displays stats
-AddPackage proxychains-ng # A hook preloader that allows to redirect TCP traffic of existing dynamically linked programs through
+AddPackage perl-ipc-system-simple # Run commands simply, with detailed
+AddPackage perl-string-shellquote # Quote strings for passing through the
+AddPackage pinta # Drawing/editing program modeled after
+AddPackage pipewire-alsa # Low-latency audio/video router and
+AddPackage pipewire-audio # Low-latency audio/video router and
+AddPackage pipewire-jack # Low-latency audio/video router and
+AddPackage pipewire-pulse # Low-latency audio/video router and
+AddPackage podman # Tool and library for running OCI-based
+AddPackage podman-compose # A script to run docker-compose.yml using
+AddPackage progress # Shows running coreutils basic commands and
+AddPackage proxychains-ng # A hook preloader that allows to redirect
 AddPackage python-pillow # Python Imaging Library (PIL) fork
-AddPackage python-poetry # Python dependency management and packaging made easy
+AddPackage python-poetry # Python dependency management and packaging
 AddPackage qemu-desktop # A QEMU setup for desktop environments
 AddPackage qt5-wayland # Provides APIs for Wayland
 AddPackage racket # A full-spectrum language with DrRacket IDE
 AddPackage ranger # Simple, vim-like file manager
-AddPackage rclone # Sync files to and from Google Drive, S3, Swift, Cloudfiles, Dropbox and Google Cloud Storage
-AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
-AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
-AddPackage rlwrap # Adds readline-style editing and history to programs
-AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage rclone # Sync files to and from Google Drive, S3,
+AddPackage reflector # A Python 3 module and script to retrieve
+AddPackage ripgrep # A search tool that combines the usability
+AddPackage rlwrap # Adds readline-style editing and history to
+AddPackage rsync # A fast and versatile file copying tool for
 AddPackage rustup # The Rust toolchain installer
 AddPackage slurp # Select a region in a Wayland compositor
 AddPackage starship # The cross-shell prompt for astronauts
+AddPackage steam # Valve's digital software delivery system
 AddPackage tealdeer # A fast tldr client in Rust
-AddPackage texinfo # GNU documentation system for on-line information and printed output
-AddPackage tree # A directory listing program displaying a depth indented list of files
-AddPackage ttf-firacode-nerd # Patched font Fira (Fura) Code from nerd fonts library
-AddPackage typst # A markup-based typesetting system for the sciences
+AddPackage texinfo # GNU documentation system for on-line
+AddPackage tree # A directory listing program displaying a
+AddPackage ttf-firacode-nerd # Patched font Fira (Fura) Code from nerd
+AddPackage typst # A markup-based typesetting system for the
 AddPackage udiskie # Removable disk automounter using udisks
 AddPackage vimiv # Image viewer with vim-like keybindings
-AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
-AddPackage wget # Network utility to retrieve files from the Web
-AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
-AddPackage yt-dlp # A youtube-dl fork with additional features and fixes
-AddPackage zathura-pdf-mupdf # PDF support for Zathura (MuPDF backend) (Supports PDF, ePub, and OpenXPS)
-AddPackage zip # Compressor/archiver for creating and modifying zipfiles
+AddPackage waybar # Highly customizable Wayland bar for Sway
+AddPackage wget # Network utility to retrieve files from the
+AddPackage wl-clipboard # Command-line copy/paste utilities for
+AddPackage yt-dlp # A youtube-dl fork with additional features
+AddPackage zathura-cb # Adds comic book support to zathura
+AddPackage zathura-pdf-mupdf # PDF support for Zathura (MuPDF backend)
+AddPackage zip # Compressor/archiver for creating and
 AddPackage zoxide # A smarter cd command for your terminal
 
 
-# Sat Aug 24 09:30:59 CST 2024 - Unknown foreign packages
+# Fri Sep  6 11:21:10 CST 2024 - Unknown foreign packages
 
 
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
-AddPackage --foreign downgrade # Bash script for downgrading one or more packages to a version in your cache or the A.L.A.
-AddPackage --foreign dragon-drop # Simple drag-and-drop source/sink for X or Wayland
+AddPackage --foreign downgrade # Bash script for downgrading one or more
+AddPackage --foreign dragon-drop # Simple drag-and-drop source/sink for X or
 AddPackage --foreign fcitx5-pinyin-custom-pinyin-dictionary # Fcitx5 
-AddPackage --foreign ghcup-hs-bin # an installer for the general purpose language Haskell
-AddPackage --foreign go-musicfox-bin # Command-line Netease Cloud Music written in Go. (Precompiled version)
-AddPackage --foreign hack-nf-mono-git # A typeface designed for source code. Patched with Nerd Fonts iconics.
+AddPackage --foreign ghcup-hs-bin # an installer for the general purpose
+AddPackage --foreign go-musicfox-bin # Command-line Netease Cloud Music written
+AddPackage --foreign hack-nf-mono-git # A typeface designed for source code.
 AddPackage --foreign jellyfin-media-player # Jellyfin Desktop Client
-AddPackage --foreign komga # Media server for comics/mangas/BDs with API and OPDS support
-AddPackage --foreign light # A program to control backlights (and other hardware lights)
-AddPackage --foreign linuxqq-nt-bwrap # New Linux QQ based on Electron, with bubblewrap sandbox and some tweaks
+AddPackage --foreign komga # Media server for comics/mangas/BDs with
+AddPackage --foreign light # A program to control backlights (and other
+AddPackage --foreign linuxqq-nt-bwrap # New Linux QQ based on Electron, with
 AddPackage --foreign llvm17 # Compiler infrastructure (LLVM 17)
-AddPackage --foreign logseq-desktop-wayland-bin # A privacy-first, open-source platform for knowledge sharing and management. Enabled flags for wayland
+AddPackage --foreign logseq-desktop-wayland-bin # A privacy-first, open-source platform for
 AddPackage --foreign neovim-drop-in # Emulate vim and vi executables with neovim
-AddPackage --foreign oils-for-unix # Our upgrade path from bash to a better language and runtime.
-AddPackage --foreign page-git # Pager powered by neovim and inspired by neovim-remote
+AddPackage --foreign oils-for-unix # Our upgrade path from bash to a better
+AddPackage --foreign page-git # Pager powered by neovim and inspired by
 AddPackage --foreign paru-bin # Feature packed AUR helper
-AddPackage --foreign quickemu # Quickly create and run optimised Windows, macOS and Linux desktop virtual machines.
-AddPackage --foreign rar # A command-line port of the rar compression utility.
-AddPackage --foreign sioyek # PDF viewer for research papers and technical books.
-AddPackage --foreign texlive-full # This packages provides texlive-full in /opt. It also tricks ArchLinux into thinking it has its texlive
-AddPackage --foreign thorium-browser-bin # Chromium fork focused on high performance and security
-AddPackage --foreign tofi # Tiny rofi / dmenu replacement for wlroots-based Wayland compositors.
+AddPackage --foreign protonlaunch # A small piece of script to launch Proton
+AddPackage --foreign quickemu # Quickly create and run optimised Windows,
+AddPackage --foreign rar # A command-line port of the rar compression
+AddPackage --foreign sioyek # PDF viewer for research papers and
+AddPackage --foreign texlive-full # This packages provides texlive-full in
+AddPackage --foreign thorium-browser-bin # Chromium fork focused on high performance
+AddPackage --foreign tofi # Tiny rofi / dmenu replacement for
 AddPackage --foreign uuplugin-bin # UU Steam Deck Plugin for General Linux
-AddPackage --foreign v2raya-bin # A web GUI client of Project V which supports VMess, VLESS, SS, SSR, Trojan, Tuic and Juicity protocols
+AddPackage --foreign v2raya-bin # A web GUI client of Project V which
 AddPackage --foreign ventoy-bin # A new bootable USB solution
 AddPackage --foreign wechat-universal-bwrap # WeChat (Universal) with bwrap sandbox
-AddPackage --foreign wl-gammarelay # A client and daemon for changing color temperature and brightness under Wayland via keybindings.
+AddPackage --foreign wl-gammarelay # A client and daemon for changing color
 AddPackage --foreign zigup-bin # Download and manage zig compilers
 
 
-# Sat Aug 24 09:30:59 CST 2024 - New / changed files
+# Fri Sep  6 11:21:10 CST 2024 - New / changed files
 
 
 CreateFile /etc/.pwd.lock 600 > /dev/null
-CopyFile /etc/NetworkManager/system-connections/212.nmconnection 600
-CopyFile /etc/NetworkManager/system-connections/502.nmconnection 600
-CopyFile /etc/NetworkManager/system-connections/502_5G.nmconnection 600
-CopyFile /etc/NetworkManager/system-connections/ShanghaiTech-Guest.nmconnection 600
-CopyFile /etc/NetworkManager/system-connections/iPhone.nmconnection 600
-CopyFile /etc/NetworkManager/system-connections/shtech.nmconnection 600
 CopyFile /etc/adjtime
 CreateLink /etc/bash_completion.d/guix /var/guix/profiles/per-user/root/current-guix/etc/bash_completion.d/guix
 CreateLink /etc/bash_completion.d/guix-daemon /var/guix/profiles/per-user/root/current-guix/etc/bash_completion.d/guix-daemon
@@ -253,7 +250,7 @@ CopyFile /etc/xml/catalog
 CreateDir /lost+found 700
 
 
-# Sat Aug 24 09:31:00 CST 2024 - New file properties
+# Fri Sep  6 11:21:11 CST 2024 - New file properties
 
 
 SetFileProperty /etc/jellyfin/jellyfin.env group jellyfin
@@ -261,39 +258,5 @@ SetFileProperty /etc/jellyfin/jellyfin.env owner jellyfin
 SetFileProperty /etc/jellyfin/logging.json group jellyfin
 SetFileProperty /etc/jellyfin/logging.json owner jellyfin
 SetFileProperty /etc/jellyfin group jellyfin
-SetFileProperty /etc/jellyfin mode 750
 SetFileProperty /etc/jellyfin owner jellyfin
 SetFileProperty /etc/v2raya mode 750
-
-
-# Tue Aug 27 21:45:13 CST 2024 - Unknown packages
-
-
-AddPackage steam # Valve's digital software delivery system
-
-
-# Wed Aug 28 01:39:26 CST 2024 - Unknown foreign packages
-
-
-AddPackage --foreign protonlaunch # A small piece of script to launch Proton
-
-
-# Wed Aug 28 01:39:26 CST 2024 - New / changed files
-
-
-CopyFile /etc/NetworkManager/system-connections/212.nmconnection 600
-CopyFile /etc/v2raya/config.json 600
-
-
-# Thu Sep  5 16:38:51 CST 2024 - Unknown packages
-
-
-AddPackage pinta # Drawing/editing program modeled after Paint.NET. It's goal is to provide a simplified alternative to
-AddPackage zathura-cb # Adds comic book support to zathura
-
-
-CopyFile /etc/NetworkManager/system-connections/212.nmconnection 600
-CopyFile /etc/v2raya/config.json 600
-
-
-SetFileProperty /etc/jellyfin mode ''
